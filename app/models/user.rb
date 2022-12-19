a93f5ae926d7c9fb20aaccd78370f91d430b771a
Enock <enock.mokua@student.moringaschool.com>
2022-12-19 20:24:42 +0300

    add model validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
+  validates :amount, length: { minimum: 0, maximum: 100_000 }
+  validates :phone, uniqueness: true
 end
