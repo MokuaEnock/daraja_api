612a4e108189b1921dbd31ff59a48ad07f5144f2
Enock <enock.mokua@student.moringaschool.com>
2022-12-20 14:28:37 +0300

    add html user email html format

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: "mokuaenock0@gmail.com"
+
+  def welcome_email
+    @user = params[:user]
+    @url = "http://"
+    mail(to: @user.email, subject: "Welcome to New Life hospital")
+  end
 end
