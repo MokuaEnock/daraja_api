1f077b53f5efa9b8c318ef5cbde92f854be0a590
Enock <enock.mokua@student.moringaschool.com>
2022-12-20 11:23:06 +0300

    reconfigure cors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  validates :username,
-            uniqueness: {
-              message: "An account associated with %{value} already exists"
-            }
-  validates :email, uniqueness: true
-  validates :amount, length: { minimum: 0, maximum: 100_000 }
-  validates :phone, uniqueness: true
+  # validates :username,
+  #           uniqueness: {
+  #             message: "An account associated with %{value} already exists"
+  #           }
+  # validates :email, uniqueness: true
+  # validates :amount, length: { minimum: 0, maximum: 100_000 }
+  # validates :phone, uniqueness: true
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,9 +8,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:8080"
-
-    resource "*",
-             headers: :any,
-             methods: %i[get post put patch delete options head]
+    resource "*", headers: :any, methods: %i[get post delete patch]
   end
 end
