f2eff2fede47a05320bd892b173ee0ead7ef30f6
Enock <enock.mokua@student.moringaschool.com>
2022-12-20 12:49:27 +0300

    prepare project for deployment on railway

--- Gemfile
+++ Gemfile
@@ -41,9 +41,10 @@ group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
 end
 
-group :development do
+group :production do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+  gem "pg"
 end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
-  validates :username,
-            uniqueness: true,
-            message: `An account associated with that username already exists`
+  # validates :username,
+  #           uniqueness: true,
+  #           message: `An account associated with that username already exists`
 
-  validates :email,
-            uniqueness: true,
-            message: "An acount with that email already exists"
-  validates :amount, length: { minimum: 0, maximum: 100_000 }
-  validates :phone,
-            uniqueness: true,
-            message:
-              `An account associated with that phone number already exists`
+  # validates :email,
+  #           uniqueness: true,
+  #           message: "An acount with that email already exists"
+  # validates :amount, length: { minimum: 0, maximum: 100_000 }
+  # validates :phone,
+  #           uniqueness: true,
+  #           message:
+  #             `An account associated with that phone number already exists`
 end
