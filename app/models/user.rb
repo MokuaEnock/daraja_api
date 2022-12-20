a7ba8fb8a357bd709896f76e204934557193854e
Enock <enock.mokua@student.moringaschool.com>
2022-12-20 11:57:48 +0300

    add message to validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   validates :username,
-            uniqueness: {
-              message: "An account associated with %{value} already exists"
-            }
-  validates :email, uniqueness: true
+            uniqueness: true,
+            message: `An account associated with that username already exists`
+
+  validates :email,
+            uniqueness: true,
+            message: "An acount with that email already exists"
   validates :amount, length: { minimum: 0, maximum: 100_000 }
-  validates :phone, uniqueness: true
+  validates :phone,
+            uniqueness: true,
+            message:
+              `An account associated with that phone number already exists`
 end
